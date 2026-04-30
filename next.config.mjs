ef04bc401045e78beedb072abf690bdd9a0af5a9
pol-dev-shinroo <manus.dream.language@gmail.com>
2026-04-30 15:37:08 +0900

    Allow all image hostnames

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,12 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'drive.google.com',
+        protocol: "https",
+        hostname: "**",
       },
     ],
   },
 };
-
 export default nextConfig;
